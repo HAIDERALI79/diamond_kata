8a90a3a20260f08d4da526edf9395fd358b71840
Haider ali <uchiha.usama@gmail.com>
2021-05-12 22:00:12 +0300

    second approach

--- diamond.dart
+++ diamond.dart
@@ -29,13 +29,12 @@ class Diamond {
   ];
   List<String> rows(String s) {
     var result = <String>[];
-    var range = alphaptic.getRange(1, alphaptic.indexOf(s) + 1);
-    var ranglength = alphaptic.indexOf(s);
-    result.insert(0,
-        '${outerSpaceLength(ranglength, alphaptic.indexOf(alphaptic.first))}${alphaptic[0]}${outerSpaceLength(ranglength, alphaptic.indexOf(alphaptic.first))}');
+    var charLength = alphaptic.indexOf(s);
+    var range = charLength != 0
+        ? alphaptic.getRange(0, alphaptic.indexOf(s) + 1)
+        : s.split('');
     for (var item in range) {
-      result.add(
-          '${outerSpaceLength(ranglength, alphaptic.indexOf(item))}$item${inerSpaceLength(alphaptic.indexOf(item))}$item${outerSpaceLength(ranglength, alphaptic.indexOf(item))}');
+      result.add(getDiamondRow(charLength, item, alphaptic.indexOf(item)));
     }
     result.addAll(List.from(result.reversed.skip(1)));
 
@@ -43,16 +42,15 @@ class Diamond {
   }
 }
 
-String inerSpaceLength(int itemValue) {
+String getDiamondRow(int charLength, String item, int itemValue) {
   String spaces;
-  if (itemValue <= 1) {
-    return spaces = List.filled(itemValue, ' ').join();
-  } else if (itemValue >= 2) {
-    return spaces = List.filled((itemValue + (itemValue - 1)), ' ').join();
+  var outerSpaceLength = List.filled((charLength - itemValue), ' ').join();
+  if (itemValue == 0) {
+    spaces = ('$outerSpaceLength$item$outerSpaceLength');
+  } else if (itemValue >= 1) {
+    var inerSpaceLength =
+        List.filled((itemValue + (itemValue - 1)), ' ').join();
+    spaces = ('$outerSpaceLength$item$inerSpaceLength$item$outerSpaceLength');
   }
   return spaces;
-}
-
-String outerSpaceLength(int length, int itemValue) {
-  return List.filled(length - itemValue, ' ').join();
 }
